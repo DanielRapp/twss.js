26729a03a1186ab5844d66c5b7f7c41a05aeaaec
Florent Jaby <florent.jaby@gmail.com>
2012-01-12 17:12:31 +0100

    add more optimist checks

--- bin/twss.js
+++ bin/twss.js
@@ -4,18 +4,47 @@ var twss = require('../index.js');
 var colors = require('colors');
 
 var argv = require('optimist')
-           .usage('twss classifies if a sentence can be replied with "that\'s what she said".')
-           .wrap(80)
+           .usage('$0 classifies if a sentence can be replied with "that\'s what she said".')
+           .wrap(50)
            .describe({
-             a: 'sets the algorithm to use. `nbc` for naive bayes classifier and `nbc` for k-nearest neighbor',
+             a: 'sets the algorithm to use. `nbc` for naive bayes classifier and `knn` for k-nearest neighbor',
              t: 'threshold. default is 0.5. values from 0 to 1',
-             p: 'pretty print. Prints input in green if it can be answered with "that\'s what she said". Always exits with 0'
+             p: 'pretty print. Prints input in green if it can be answered with "that\'s what she said". Always exits with 0',
+             h: "you're staring at it"
            })
            .boolean('p')
            .alias('a', 'algo')
            .alias('t', 'threshold')
            .alias('p', 'pretty-print')
+           .alias('h', 'help')
+           .check(checkArguments)
            .argv
+
+function checkArguments (argv) {
+    if(argv.threshold) {
+        if('number' !== typeof argv.threshold) {
+            throw new TypeError('threshold must be a number');
+        }
+        else if(argv.threshold < 0 || argv.threshold > 1) {
+            throw new RangeError('threshold must be between 0 and 1');
+        }
+    }
+    if(argv.algo) {
+        switch(argv.algo) {
+            case 'knn':
+            case 'nbc':
+                break;
+            default:
+                throw new SyntaxError('algo must be one of: knn, nbc')
+                break;
+        }
+    }
+}
+
+if(argv.help) {
+    require('optimist').showHelp();
+    process.exit();
+}
 
 var sentences = argv._;
 
